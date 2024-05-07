f0157d3f7bae76bb9241e7df315362e3b6f34da1
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-05-07 14:56:59 +0200

    Attempt to rerote on 404

---
+++ src/routes/404.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        if (location.href.includes(".html")) return;
+        location.href += "index.html";
+    });
+</script>
